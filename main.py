79bb3c40846c6df1c96d2d48620b3a4b6ad8b520
mousescorpion <88336247+mousescorpion@users.noreply.github.com>
2026-04-20 09:18:38 -0700

    hm?

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 from google.cloud import datastore
 import json
 
@@ -7,7 +7,12 @@ datastore_client = datastore.Client()
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    query = datastore_client.query(kind='Set')
+    sets = []
+    for entity in query.fetch():
+        cardset = dict(entity)
+        sets.append(cardset)
+    return render_template('home.html', sets=sets)
 
 @app.route('/create')
 def create():
@@ -15,11 +20,32 @@ def create():
 
 @app.route('/create/get_name/<the_name>',methods=['GET'])
 def get_name(the_name):
-    print(the_name)
-    key=datastore_client.key("Name", the_name)
+    key=datastore_client.key("Set", the_name)
     if datastore_client.get(key) is None:
         return "T"
     return "F"
+
+@app.route('/view_set/<set_id>',methods=['GET'])
+def view_set(set_id):
+    key = datastore_client.key('Set', set_id)
+    entity = datastore_client.get(key)
+
+    if not entity:
+        return "Item not found", 404
+
+    item = dict(entity)
+    return render_template('view_set.html', item=item)
+
+@app.route('/flashcards/<set_id>',methods=['GET'])
+def flashcards(set_id):
+    key = datastore_client.key('Set', set_id)
+    entity = datastore_client.get(key)
+
+    if not entity:
+        return "Item not found", 404
+
+    item = dict(entity)
+    return render_template('flashcards.html', item=item)
 
 
 @app.route('/create/add_data',methods=['POST', 'GET'])
@@ -27,6 +53,7 @@ def add_data():
     name="noName"
     description="no description"
     data={}
+    lengths=[]
     for key, value in request.form.items():
         if key=="name":
             name=value
@@ -34,14 +61,22 @@ def add_data():
             description=value
         else:
             data.update({key: value})
-    key = datastore_client.key("Name", name)
+            if len(lengths)< (int(data[key][1:(data[key].index(':'))])):
+                lengths.append(1)
+            else:
+                lengths[int(data[key][1:(data[key].index(':'))])]=int(data[key][((data[key].index(':'))+1):])
+    print(lengths)
+
+    key = datastore_client.key("Set", name)
     entity=datastore.Entity(key=key)
     entity.update({
         'name': name,
-        'description': description
+        'description': description,
+        'data': data,
+        'lengths': lengths
     })
     datastore_client.put(entity)
-    return render_template('home.html')
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
